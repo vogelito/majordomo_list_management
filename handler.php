6233b74e076e91c38b628e6afc3af975ce9f1b51
Daniel Vogel <dvogel@cs.stanford.edu>
2014-09-09 01:18:39 -0400

    simple php scripts to unsubscribe users

---
+++ handler.php
@@ -0,0 +1,40 @@
+<?php
+  include("vars.php");
+  include("crypto.php");
+  $key = base64_decode($key);
+
+  $subject = "";
+  $send_to = "";
+  $message = "";
+  $display = "";
+
+  if(isset($_POST["email"])) {
+    // This is coming from the web-form
+    $send_to = $_POST["email"];
+    $subject = "Unsubscribe from " . $club_name . "Mailing List";
+    $iv = getIV();
+    $encrypted_email = encode($send_to, $key, $iv);
+    $link = $http_path . "?e=" . $encrypted_email . "&iv=" . $iv;
+    $message = "To unsubscribe from the " . $club_name . " mailing list, please follow this link: " . $link;
+    $display = "Please check your e-mail";
+  } elseif(isset($_GET["e"]) && isset($_GET["iv"])) {
+    // This is coming from the confirmation e-mail
+    $send_to = $majordomo_email;
+    $e = $_GET["e"];
+    $iv = $_GET["iv"];
+    $unsub_email = decode($e, $key, $iv);
+    $message = "APPROVE	" . $mailing_list_password . "	UNSUBSCRIBE	" . $listserve . "	" . $unsub_email;
+    $display = "Unsubscribe Request Sent";
+  } else {
+    echo "An error occurred";
+    exit();
+  }
+
+  $headers = 'From: "Pizza Master" <foodmaster@vogelito.com>' . "\r\n" .
+    'Reply-To: dvogel@cs.stanford.edu' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  mail($send_to, $subject, $message, $headers);
+
+  echo $display;
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  include("vars.php");
+?>
+<html>
+<head><title><?php echo $club_name; ?> Mailing List Unsubscribe</title></head>
+<body>
+  <h1><center><?php echo $club_name; ?> Mailing List Unsubscribe!</center></h1>
+  <form action="handler.php" method="post">
+  E-mail to remove: <input type="text" name="email"><br>
+  <input type="submit">
+  </form>
+
+</body>
+</html>
